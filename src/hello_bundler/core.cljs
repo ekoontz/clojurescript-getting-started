50dc47f39987b3cdc08d359b85082620d1497ab4
Eugene Koontz <ekoontz@hiro-tan.org>
2024-12-01 13:54:39 +0100

    use a React version 17 attempt at rendering (doesn't work)

--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,7 +1,15 @@
 (ns hello-bundler.core
   (:require [react]
+            [react-dom]
             [mapbox-gl]))
 
 (.log js/console (str "hello."))
 (.log js/console react/Component)
 (.log js/console (str "goodbye."))
+
+
+(.render js/ReactDOM
+         (.createElement js/React "h2" nil "This part is dynamically generated.")
+         (.getElementById js/document "app"))
+
+
