a6ba4776ea4e0a41318bfffc90cfecfddf8b311c
Eugene Koontz <ekoontz@hiro-tan.org>
2024-12-01 14:03:33 +0100

    you have to double-quote react-dom/client to require it

--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,15 +1,18 @@
 (ns hello-bundler.core
   (:require [react]
             [react-dom]
+            ["react-dom/client" :refer [createRoot]]
             [mapbox-gl]))
 
 (.log js/console (str "hello."))
 (.log js/console react/Component)
 (.log js/console (str "goodbye."))
 
+(.log js/console (str "app element id: " (.getElementById js/document "app")))
+;;(.log js/console (str "js/ReactDOM: " js/ReactDOM))
 
-(.render js/ReactDOM
-         (.createElement js/React "h2" nil "This part is dynamically generated.")
-         (.getElementById js/document "app"))
+;;(.render js/ReactDOM
+;;         (.createElement js/React "h2" nil "This part is dynamically generated.")
+;;         (.getElementById js/document "app"))
 
 
