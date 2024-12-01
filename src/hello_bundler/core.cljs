a6bf6f06741fe40cdcd3987bb04b19adf2002564
Eugene Koontz <ekoontz@hiro-tan.org>
2024-12-01 17:20:22 +0100

    can successfully create a createRoot

--- src/hello_bundler/core.cljs
+++ src/hello_bundler/core.cljs
@@ -1,12 +1,14 @@
 (ns hello-bundler.core
   (:require [react]
             [react-dom]
-            ["react-dom/client" :refer [createRoot]]
+            ["react-dom/client" :refer [createRoot] :as rdc]
             [mapbox-gl]))
 
 (.log js/console (str "hello."))
 (.log js/console react/Component)
 (.log js/console (str "goodbye."))
+(.log js/console (str "rdc: " rdc))
+(.log js/console (str "createRoot: " createRoot))
 
 (.log js/console (str "app element id: " (.getElementById js/document "app")))
 ;;(.log js/console (str "js/ReactDOM: " js/ReactDOM))
